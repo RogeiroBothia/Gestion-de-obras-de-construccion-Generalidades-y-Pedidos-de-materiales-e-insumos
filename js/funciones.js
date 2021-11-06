e179343ca0a2b28783a0ea0b00e7d6584677a205
Yuber Eduardo Lobo Quintero <yuberlobo13@gmail.com>
2021-11-05 23:38:48 -0500

    Implementation Administrador Interventor

--- js/funciones.js
+++ js/funciones.js
@@ -2,10 +2,10 @@ var menuCerrado = false;
 var tamañoPage = 0;
 
 function menu() {
-    if(tamañoPage>600 || tamañoPage == 0){
-        if(menuCerrado==false){
+    if (tamañoPage > 600 || tamañoPage == 0) {
+        if (menuCerrado == false) {
             menuCerrado = true;
-        }else{
+        } else {
             menuCerrado = false;
         }
         const text_nav = document.getElementById("texto-nav");
@@ -18,57 +18,71 @@ function menu() {
         menu_figure.classList.toggle("menu-figure-active");
         const menu_ul = document.querySelector(".menu-ul");
         menu_ul.classList.toggle("menu-ul-active");
-    
+
         const contenedor_principal = document.querySelector(".contenedor-principal");
         contenedor_principal.classList.toggle("contenedor-principal-active");
-    
+
         var menu_a = document.getElementsByClassName("menu-a");
-        for(let i = 0; i < menu_a.length; i++){
+        for (let i = 0; i < menu_a.length; i++) {
             menu_a[i].classList.toggle("menu-a-active");
         }
-    
+
         var menu_li = document.getElementsByClassName("menu-li");
-        for(let i = 0; i < menu_li.length; i++){
+        for (let i = 0; i < menu_li.length; i++) {
             menu_li[i].classList.toggle("menu-li-active");
         }
-    
+
         const menu_info = document.getElementById("menu_info");
-    
+
         const footer = document.getElementById("footer");
-    
-        
-        if(window.innerWidth > 600){
-            if(menuCerrado==true){
+
+
+        if (window.innerWidth > 600) {
+            if (menuCerrado == true) {
                 footer.classList.toggle("footer_active");
                 footer.classList.remove("footer_desactive");
-            }else{
+            } else {
                 footer.classList.remove("footer_active");
                 footer.classList.toggle("footer_desactive");
             }
         }
-        
-    
-        if(menu_info.innerHTML === "") {
-            menu_info.innerHTML = '<h4 class="menu-text-user my-2">USUARIO</h4><h5 class="menu-cargo my-2">SUPERVISOR</h5><h6 class="menu-text-email my-2"><a href="mailto:user@gmail.com">user@gmail.com</a></h6>';
-        } else { menu_info.innerHTML = "" }
-        
+
+        let name_doc = document.title;
+
+        switch (name_doc) {
+            case "Sistema Supervisor Principal":
+                if (menu_info.innerHTML === "") {
+                    menu_info.innerHTML = '<h4 class="menu-text-user my-2">USUARIO</h4><h5 class="menu-cargo my-2">SUPERVISOR</h5><h6 class="menu-text-email my-2"><a href="mailto:user@gmail.com">user@gmail.com</a></h6>';
+                } else { menu_info.innerHTML = "" }
+                break;
+            case "Sistema Interventor Principal":
+                if (menu_info.innerHTML === "") {
+                    menu_info.innerHTML = '<h4 class="menu-text-user my-2">USUARIO</h4><h5 class="menu-cargo my-2">INTERVENTOR</h5><h6 class="menu-text-email my-2"><a href="mailto:user@gmail.com">user@gmail.com</a></h6>';
+                } else { menu_info.innerHTML = "" }
+                break;
+            case "Sistema Administrador Principal":
+                if (menu_info.innerHTML === "") {
+                    menu_info.innerHTML = '<h4 class="menu-text-user my-2">USUARIO</h4><h5 class="menu-cargo my-2">ADMINISTRADOR</h5><h6 class="menu-text-email my-2"><a href="mailto:user@gmail.com">user@gmail.com</a></h6>';
+                } else { menu_info.innerHTML = "" }
+                break;
+        }
     }
 }
 
-window.addEventListener("resize", function(){
+window.addEventListener("resize", function () {
     tamañoPage = window.innerWidth;
-    if(window.innerWidth < 600){
+    if (window.innerWidth < 600) {
         const footer = document.getElementById("footer");
         footer.classList.add("footer_600");
     }
-    
-    if(window.innerWidth > 600){
+
+    if (window.innerWidth > 600) {
         const footer = document.getElementById("footer");
         footer.classList.remove("footer_600");
     }
 });
 
-if(window.innerWidth < 600){
+if (window.innerWidth < 600) {
     const footer = document.getElementById("footer");
     footer.classList.add("footer_600");
 }
